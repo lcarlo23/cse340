62430d7a7ca787f9d70a2fb45b6f801103c9d440
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-03-25 17:00:06 +0100

    Complete data validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -16,6 +17,8 @@ router.get(
 // Route post account
 router.post(
   "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount),
 );
 

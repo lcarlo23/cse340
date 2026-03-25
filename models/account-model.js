028629cf586edadd1d757f701e06b71de5ff6a63
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-03-25 17:43:55 +0100

    complete team activity

--- models/account-model.js
+++ models/account-model.js
@@ -23,4 +23,30 @@ async function registerAccount(
   }
 }
 
-module.exports = { registerAccount };
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email) {
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1";
+    const email = await pool.query(sql, [account_email]);
+    return email.rowCount;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* **********************
+ *   Check for correct password
+ * ********************* */
+async function checkPassword(account_password) {
+  try {
+    const sql = "SELECT * FROM account WHERE account_password = $1";
+    const email = await pool.query(sql, [account_password]);
+    return email.rowCount;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+module.exports = { registerAccount, checkExistingEmail, checkPassword };
